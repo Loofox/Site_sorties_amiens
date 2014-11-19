c982f239e03d0290388339832bb3f1ee6c761d0a
Florian <flo.cartier@hotmail.fr>
2014-11-19 08:22:55 +0100

    Modification du template main et des parametres de connexions a la BD

--- mini-fwk/conf/Params.ini.php
+++ mini-fwk/conf/Params.ini.php
@@ -2,9 +2,9 @@
 
 define("DB_HOST","localhost");
 define("DB_USER","root");
-define("DB_PASS","root");//""
-define("BASE","OMGL3");
-define("PORT","8089");//3306
+define("DB_PASS","");//""
+define("BASE","site");
+define("PORT","3306");//3306
 
 define('DEBUG',0);
 define('CLASSES',dirname($_SERVER["SCRIPT_FILENAME"])."/tables");
